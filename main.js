81935817d8f7cba3252be65c23a405ecb7f4fee9
Mayur Bonde <76271761+eziomb4@users.noreply.github.com>
2023-08-25 11:43:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+alert("This Web Application Is Under Development")
+// let Name = prompt("Enter Your Name ");
+// console.log(Name);
+// let pass = prompt("Enter Password");
+// console.log(pass);
+function maps(){
+    window.location.replace("https://www.google.com/maps")
+    window.history.go(-1)
+
+}
+function youtube(){
+    window.open("https://www.youtube.com/" , "_blank")
+}
+function home() {
+    alert("Press Home");
+}
+document.getElementById("Forward").onclick= function() {
+    location.href = "secondpage.html";
+}
+document.getElementById("Backward").onclick= function() {
+    window.history.back()
+}
+document.getElementById("Home").onclick= function() {
+    location.href="main.html";
+}
